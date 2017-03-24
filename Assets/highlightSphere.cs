ebff61295ef30b42c2ce080324d325d44ac62bad
Russell Alleen-Willems <ralleenwillems@gmail.com>
2017-03-24 12:30:53 -0700

    Redone VR with VRTK controls

---
+++ Assets/highlightSphere.cs
@@ -0,0 +1,47 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using System.Collections;
+
+    public class highlightSphere : VRTK_InteractableObject
+    {
+        float spinSpeed = 0f;
+        Transform rotator;
+
+        public override void StartUsing(GameObject usingObject)
+        {
+            //SceneManager.LoadScene("01_Present", LoadSceneMode.Single);
+
+            //Advance to next scene
+            int level = SceneManager.GetActiveScene().buildIndex;
+            level++;
+
+            int MaxLevels = SceneManager.sceneCountInBuildSettings;
+
+            //if level is greater than included scene indexes, reset to 0
+            if(level >= MaxLevels)
+            {
+                level = 0;
+            }
+            SceneManager.LoadScene(level, LoadSceneMode.Single);
+            Debug.Log("Scene changed");
+            
+        }
+
+        public override void StopUsing(GameObject usingObject)
+        {
+            
+        }
+
+        protected void Start()
+        {
+            
+        }
+
+        protected override void Update()
+        {
+          
+        }
+    }
+}
---
+++ Assets/showDialog.cs
@@ -0,0 +1,51 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using System.Collections;
+
+    public class showDialog : VRTK_InteractableObject
+    {
+        public override void StartUsing(GameObject usingObject)
+        {
+            /* Renderer dialogRenderer = gameObject.GetComponentInChildren<Renderer>();
+             dialogRenderer.enabled = true;
+             */
+
+            //AudioSource audioLog = gameObject.GetComponent<AudioSource>();
+
+           // audioLog.Play();
+
+            GameObject dialog = this.transform.Find("ObjectDataUI").gameObject;
+
+            bool dialogState = dialog.activeSelf;
+            if (dialogState)
+            {
+                dialog.SetActive(false);
+            } else
+            {
+                dialog.SetActive(true);
+            }
+
+           
+
+            Debug.Log("Show dialog");
+            
+        }
+
+        public override void StopUsing(GameObject usingObject)
+        {
+            
+        }
+
+        protected void Start()
+        {
+            
+        }
+
+        protected override void Update()
+        {
+          
+        }
+    }
+}
