9748d6a480068270de9c5f70039da0a8ccb60f2f
Russell Alleen-Willems <ralleenwillems@gmail.com>
2017-04-08 02:50:51 -0700

    Added all 3D models for 1884, 1902, 1925, and 2017 (from OpenStreetMap geometry). Added script and spheres to toggle models on/off.

---
+++ Assets/Show1884.cs
@@ -0,0 +1,47 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using System.Collections;
+
+    public class Show1884: VRTK_InteractableObject
+    {
+        public GameObject go_Time;
+
+
+        public override void StartUsing(GameObject usingObject)
+        { 
+            GameObject timePeriod = go_Time.gameObject;
+
+            bool timePeriodState = timePeriod.activeSelf;
+            if (timePeriodState)
+            {
+                timePeriod.SetActive(false);
+            }
+            else
+            {
+                timePeriod.SetActive(true);
+            }
+
+
+
+            Debug.Log("Show 1884");
+
+        }
+
+        public override void StopUsing(GameObject usingObject)
+        {
+
+        }
+
+        protected void Start()
+        {
+
+        }
+
+        protected override void Update()
+        {
+
+        }
+    }
+}
