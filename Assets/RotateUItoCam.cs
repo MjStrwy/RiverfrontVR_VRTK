e8dfcdef71f942991c1296c27baa65d00f6f8d1e
Russell Alleen-Willems <ralleenwillems@gmail.com>
2017-04-11 10:14:22 -0700

    Adjusted height of Data Points, Added Text Canvases to Time Period Spheres

---
+++ Assets/RotateUItoCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateUItoCam : MonoBehaviour {
+
+    public GameObject hmd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 cameraPosition = hmd.transform.position;
+        cameraPosition.y = transform.position.y;
+        transform.LookAt(cameraPosition);
+	}
+}
